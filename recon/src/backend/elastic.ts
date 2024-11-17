5149ce145a35a370d850cdfa176cedf1e2f152ed
javier-twj <javier.twj2@gmail.com>
2024-11-17 22:35:24 +0800

    refactor: simplify search

--- recon/src/backend/elastic.ts
+++ recon/src/backend/elastic.ts
@@ -2,64 +2,49 @@ import { Client } from '@elastic/elasticsearch'
 
 const indices = ['submission'] as const
 
-class ElasticController {
-  readonly clients: Record<string, Client> = {}
-
-  private async createElasticClient(elasticHost: string) {
-    const client = new Client({
-      node: elasticHost,
+function createIndex(client: Client, index: string) {
+  if (!(client.indices.exists({ index })))
+    client.indices.create({
+      index,
+      mappings: {
+        dynamic_templates: [
+          {
+            whitespace_lowercase: {
+              match_mapping_type: 'string',
+              mapping: { type: 'text' },
+            },
+          },
+          {
+            longs: {
+              match_mapping_type: 'long',
+              mapping: { type: 'float' },
+            },
+          },
+        ],
+      },
+      settings: {
+        analysis: {
+          analyzer: {
+            default: {
+              type: 'custom',
+              tokenizer: 'whitespace',
+              filter: ['lowercase'],
+            },
+          },
+        },
+        mapping: {
+          total_fields: { limit: 10000 },
+        },
+      },
     })
-    this.clients[elasticHost] = client
-    await Promise.all(
-      indices.map((index) => ElasticController.createIndex(client, index)),
-    )
-    return client
-  }
-
-  private static async createIndex(client: Client, index: string) {
-    if (!(await client.indices.exists({ index })))
-      client.indices.create({
-        index,
-        mappings: {
-          dynamic_templates: [
-            {
-              whitespace_lowercase: {
-                match_mapping_type: 'string',
-                mapping: { type: 'text' },
-              },
-            },
-            {
-              longs: {
-                match_mapping_type: 'long',
-                mapping: { type: 'float' },
-              },
-            },
-          ],
-        },
-        settings: {
-          analysis: {
-            analyzer: {
-              default: {
-                type: 'custom',
-                tokenizer: 'whitespace',
-                filter: ['lowercase'],
-              },
-            },
-          },
-          mapping: {
-            total_fields: { limit: 10000 },
-          },
-        },
-      })
-  }
-
-  async getElasticClient(elasticHost: string) {
-    if (this.clients[elasticHost]) return this.clients[elasticHost]
-    return this.createElasticClient(elasticHost)
-  }
 }
 
-const elasticController = new ElasticController()
+if (!process.env.ELASTIC_BASEURL) throw new Error("Invalid ELASTIC_BASEURL environment variable")
+const elasticClient = new Client({
+  node:process.env.ELASTIC_BASEURL
+})
 
-export default elasticController
+indices.map((index) => createIndex(elasticClient, index))
+
+export default elasticClient
 
