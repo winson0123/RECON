d316c7ffcf13521c1d919814d0dd933b233a5e7f
BaconPotatoCat <kobinfong11@gmail.com>
2024-12-01 13:35:32 +0800

    refactor: header style

--- recon/src/components/navbar/nav-bar.tsx
+++ recon/src/components/navbar/nav-bar.tsx
@@ -5,7 +5,7 @@ import SettingsButton from "./settings-button"
 export default function NavBar() {
   return (
     <nav className="mb-3 mt-0 flex items-center justify-between bg-secondary px-4 py-2">
-      <div className="text-primary"><a href='/'>RECON</a></div>
+      <div className="text-primary text-xl font-semibold"><a href='/'>RECON</a></div>
       <div className="flex flex-grow justify-center">
         <SearchBar />
       </div>
